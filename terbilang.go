335d799502c8a96662ef358d8f48ff66adc51500
Dimas Restu H <dimas.restu@student.upi.edu>
2020-11-04 01:09:33 +0700

    update partial refactor

--- terbilang.go
+++ terbilang.go
@@ -22,10 +22,9 @@ func ToTerbilang(angka string) (string, error) {
 
 	// Loop Angka String dan Konversi
 	for i := 0; i < lenAngka; i++ {
-		if i == (lenAngka-1) && (lenAngka-1) == 0 {
-			// Proses Nol
-			return "nol", nil
-		}
+		// Cari Posisi Digit
+		posDigit := (lenAngka - 1) - i
+		grpDigit := posDigit % 3
 
 		// Konversi Angka String ke Angka Int
 		intAngka, err := strconv.Atoi(string(strAngka[i]))
@@ -34,78 +33,80 @@ func ToTerbilang(angka string) (string, error) {
 		}
 
 		// Konversi Angka ke Bilangan
-		if arrAngka[intAngka] != "" {
+		switch intAngka {
+		case 1:
+			switch grpDigit {
+			case 2:
+				// Proses Seratus
+				resTerbilang += "seratus "
+
+			case 1:
+				// Konversi Angka String Selanjutnya ke Angka Int Selanjutnya
+				nextIntAngka, err := strconv.Atoi(string(strAngka[i+1]))
+				if err != nil {
+					return "", err
+				}
+
+				switch nextIntAngka {
+				case 0:
+					// Proses Sepuluh
+					resTerbilang += "sepuluh "
+
+				case 1:
+					// Proses Sebelas
+					resTerbilang += "sebelas "
+
+				default:
+					// Proses Belasan
+					resTerbilang += arrAngka[nextIntAngka] + " belas "
+				}
+
+				// Skip Angka Selanjutnya
+				i++
+
+				// Cari Ulang Posisi Digit
+				posDigit--
+				grpDigit--
+
+			case 0:
+				if i == (lenAngka-1) || (lenAngka-1) > 4 {
+					// Proses Satu
+					resTerbilang += arrAngka[intAngka] + " "
+				} else {
+					// Proses Seribu
+					resTerbilang += "seribu "
+					continue
+				}
+			}
+
+		case 0:
+			if i == (lenAngka-1) && (lenAngka-1) == 0 {
+				// Proses Nol
+				return "nol", nil
+			}
+			continue
+
+		default:
 			resTerbilang += arrAngka[intAngka] + " "
-		} else {
-			resTerbilang += arrAngka[intAngka]
 		}
 
-		// Cari Posisi Digit
-		posDigit := (lenAngka - 1) - i
-		grpDigit := posDigit % 3
-
-		if intAngka != 0 {
+		if intAngka > 1 {
 			switch grpDigit {
 			case 2:
 				// Proses Ratusan
-				if intAngka > 1 {
-					resTerbilang += "ratus "
-				} else if intAngka == 1 {
-					// Cara Bar-Bar Hapus Konversi "Satu"
-					// Dan Append Bentuk Baru :D
-					resTerbilang = strings.TrimSuffix(resTerbilang, arrAngka[intAngka]+" ")
-					resTerbilang += "seratus "
-				}
+				resTerbilang += "ratus "
+
 			case 1:
 				// Proses Puluhan
-				if intAngka > 1 {
-					resTerbilang += "puluh "
-				} else if intAngka == 1 {
-					// Konversi Angka String Selanjutnya ke Angka Int Selanjutnya
-					nextIntAngka, err := strconv.Atoi(string(strAngka[i+1]))
-					if err != nil {
-						return "", err
-					}
-
-					// Cara Bar-Bar Hapus Konversi "Satu"
-					// Dan Append Bentuk Baru :D
-					resTerbilang = strings.TrimSuffix(resTerbilang, arrAngka[intAngka]+" ")
-					switch nextIntAngka {
-					case 0:
-						// Proses Sepuluh
-						resTerbilang += "sepuluh "
-					case 1:
-						// Proses Sebelas
-						resTerbilang += "sebelas "
-					default:
-						// Proses Belasan
-						resTerbilang += arrAngka[nextIntAngka] + " belas "
-					}
-
-					// Skip Angka Selanjutnya
-					i++
-
-					// Cari Ulang Posisi Digit
-					posDigit = (lenAngka - 1) - i
-					grpDigit = posDigit % 3
-				}
+				resTerbilang += "puluh "
 			}
 		}
 
 		if grpDigit == 0 {
-			grpUnits := posDigit / 3
-			if i == 0 && intAngka == 1 && grpUnits <= 1 {
-				// Cara Bar-Bar Hapus Konversi "Satu"
-				// Dan Append Bentuk Baru :D
-				resTerbilang = strings.TrimSuffix(resTerbilang, arrAngka[intAngka]+" ")
-				resTerbilang += "se" + arrUnits[grpUnits] + " "
-			} else {
-				resTerbilang += arrUnits[grpUnits] + " "
-			}
+			resTerbilang += arrUnits[posDigit/3] + " "
 		}
 	}
 
-	// Trim Hasil Konversi dan Return Hasil
-	resTerbilang = strings.TrimSpace(resTerbilang)
-	return resTerbilang, nil
+	// Trim Hasil Konversi dan Return
+	return strings.TrimSpace(resTerbilang), nil
 }
